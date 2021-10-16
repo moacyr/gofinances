1987b22af0ed47286a40edc3512acc17c215c333
Moacyr Rodrigues Pereira <moacyr@m3sistemas.com.br>
2021-10-16 09:55:25 -0300

    colocando fontes

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: "https://github.com/moacyr.png" }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Moacyr</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }

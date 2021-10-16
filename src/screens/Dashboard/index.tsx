528241fb99b57b763b9d9daf19004d727c42d00f
Moacyr Rodrigues Pereira <moacyr@m3sistemas.com.br>
2021-10-16 16:19:57 -0300

    Lista de transacoes

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+
 import {
   Container,
   Header,
@@ -11,9 +14,54 @@ import {
   UserWrapper,
   Icon,
   HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+
+  const data: DataListProps[] = [{
+    id: '1',
+    type: 'positive',
+    title: "Desenvolvimento de sistemas",
+    amount: "'00000",
+    category: {
+      name: "vendas",
+      icon: "dollar-sign"
+    },
+    date: "dd/dd/dddd"
+  },
+  {
+    id: '2',
+    type: 'negative',
+    title: "gasto de sistemas",
+    amount: "'00000",
+    category: {
+      name: "Alimentação",
+      icon: "coffee"
+    },
+    date: "dd/dd/dddd"
+  }
+    ,
+  {
+    id: '3',
+    type: 'negative',
+    title: "gastp de sistemas",
+    amount: "'00000",
+    category: {
+      name: "casa",
+      icon: "shopping-bag"
+    },
+    date: "dd/dd/dddd"
+  }
+
+  ];
+
   return (
     <Container>
       <Header>
@@ -49,6 +97,17 @@ export function Dashboard() {
           lastTransaction="01 à 16 de abril"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+
+      </Transactions>
+
     </Container>
   )
 }
